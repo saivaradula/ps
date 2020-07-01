337b72c5a6520879578c09719b80a41ec1d8e5a4
Saibaba Varadula <svaradula@mediaamp.org>
2020-07-01 14:38:39 +0530

    publishing

---
+++ build/precache-manifest.a589a71dca7ce13131695cc523e3e3a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "850b289df1a0a26ca0112f13f4b9c32a",
+    "url": "/ps/index.html"
+  },
+  {
+    "revision": "5c46e78a116127014315",
+    "url": "/ps/static/css/main.9bd9cf8e.chunk.css"
+  },
+  {
+    "revision": "2aae4f3ef64d348ea537",
+    "url": "/ps/static/js/2.355438ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ps/static/js/2.355438ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c46e78a116127014315",
+    "url": "/ps/static/js/main.0f818eae.chunk.js"
+  },
+  {
+    "revision": "0a882ba49ec787e14a1a",
+    "url": "/ps/static/js/runtime-main.d36a31a8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ps/static/media/logo.5d5d9eef.svg"
+  }
+]);
